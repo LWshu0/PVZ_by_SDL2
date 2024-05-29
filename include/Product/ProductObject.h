7e40d0a450f75f2ea3d7904b48b68f8fab4f9ce7
Liuwsh <eastwind10274@163.com>
2024-05-29 21:27:31 +0800

    feat: product增加一个状态, 后续添加细节

--- include/Product/ProductObject.h
+++ include/Product/ProductObject.h
@@ -6,6 +6,12 @@
 #include "Core/GameObject.h"
 #include "Core/GlobalVars.h"
 
+enum ProductState {
+    Product_MOVE,         // 运动状态
+    Product_BREAK,        // 破碎动画
+    Product_DELETE        // 删除状态, 其对象可以被销毁
+};
+
 enum ProductType {
     // Bullet
     PeaType,
@@ -16,11 +22,13 @@ enum ProductType {
 
 class ProductObject : public GameObject {
 protected:
+    ProductState m_state;
     std::shared_ptr<ObjectMotion> m_updater;
 public:
     ProductObject(const SDL_FRect& aabb);
 
     virtual std::shared_ptr<ProductObject> clone(float x, float y) = 0;
+    inline ProductState getState() { return m_state; }
     virtual ProductType getType() = 0;
     virtual int update() override
     {
--- src/Product/ProductObject.cpp
+++ src/Product/ProductObject.cpp
@@ -1,7 +1,8 @@
 #include "Product/ProductObject.h"
 ProductObject::ProductObject(const SDL_FRect& aabb) :
     GameObject(aabb),
-    m_updater(nullptr)
+    m_updater(nullptr),
+    m_state(ProductState::Product_MOVE)
 {}
 
 ProductObject::~ProductObject()
